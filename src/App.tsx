64dc03ee0d27820c97cbc9e0a011b65c4b74e5ec
eli5768356 <elimatarran@gmail.com>
2026-02-06 14:03:51 +0100

    Cambiado el estilo de la página

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,56 @@ function Login() {
   return (
     <>
       <header>
-        <a href="/inicio">← Volver al Inicio</a> 
+        <a href="/inicio">
+          <span>←</span> Volver al Inicio
+        </a>
       </header>
-      <div className="login-container">
-        <CustomImage src="src/assets/logo.png" alt="Logo de Deportes" className="logo"/>
-        <CustomTitle text="Iniciar Sesión"/>
-        <CustomSubtitle text="Inicia sesión para acceder a todos nuestros servicios."/>
-        <form>
-          <CustomInput labelText="Correo Electrónico:" 
-            inputType="email" 
-            inputId="email" 
-            inputName="email" 
-            isRequired={true} 
-            placeholder="ejemplo@correo.com"/> 
-          
-          <CustomInput labelText="Contraseña:"
-            inputType="password" 
-            inputId="password" 
-            inputName="password" 
-            isRequired={true} 
-            placeholder="Ingresa tu contraseña..."/>
-          
-          <button type="submit">Iniciar Sesión</button>
-        </form>
 
-        <p>¿No tienes una cuenta? <a href="/registro" className="register-link">Regístrate aquí</a></p>
+      <div className="login-page-wrapper">
+        <div className="login-container">
+          <div className="logo-container">
+            <CustomImage
+              src="src/assets/logo.png"
+              alt="Logo de Desportes"
+              className="logo"
+            />
+          </div>
+
+          <CustomTitle text="Iniciar Sesión" />
+          <CustomSubtitle text="Accede a tu cuenta para reservar tus canchas favoritas." />
+
+          <form>
+            <CustomInput
+              labelText="Correo Electrónico"
+              inputType="email"
+              inputId="email"
+              inputName="email"
+              isRequired={true}
+              placeholder="nombre@ejemplo.com"
+            />
+
+            <CustomInput
+              labelText="Contraseña"
+              inputType="password"
+              inputId="password"
+              inputName="password"
+              isRequired={true}
+              placeholder="••••••••"
+              labelLink={{
+                text: "¿Olvidaste tu contraseña?",
+                href: "/recuperar",
+                className: "forgot-password-link"
+              }}
+            />
+
+            <button type="submit">Entrar ahora</button>
+          </form>
+
+          <p className="footer-text">
+            ¿Aún no tienes cuenta?
+            <a href="/registro" className="register-link">Crea una aquí</a>
+          </p>
+        </div>
       </div>
     </>
   )

d30491aebfcbe60dd68d8194e1d7c1e66d4ee8ac
eli5768356 <elimatarran@gmail.com>
2026-03-16 09:21:07 +0100

    Changes to be committed: 	modified:   src/pages/ContactUs.tsx 	modified:   src/router/app.router.tsx

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from 'react-router'
+import { createBrowserRouter, Navigate, Outlet, ScrollRestoration } from 'react-router'
 import Login from '../pages/Login';
 import LandingPage from '../pages/LandingPage';
 import { PrivateRoute } from './private.routes';
@@ -10,43 +10,57 @@ import { TermsandConds } from '../pages/TermsAndConds';
 import SignIn from '../pages/Register';
 import { PrivacyPolicy } from '../pages/PrivacyPolicy';
 
+const RootLayout = () => {
+    return (
+        <>
+            <ScrollRestoration />
+            <Outlet />
+        </>
+    );
+};
+
 export const Router = createBrowserRouter(
     [
         {
-            path: '/',
-            element: <LandingPage />
-        },
-        {
-            path: "/registro",
-            element: <GuestRoute element={<SignIn />} />
-        },
-        {
-            path: "/login",
-            element: <GuestRoute element={<Login />} />
-        },
-        {
-            path: "/reservar",
-            element: <Reservas />
-        },
-        {
-            path: "/perfil",
-            element: <PrivateRoute element={<UserProfile />} />
-        },
-        {
-            path: "/contacto",
-            element: <ContactUs />
-        },
-        {
-            path: "/terminos",
-            element: <TermsandConds />
-        },
-        {
-            path: "/politica-privacidad",
-            element: <PrivacyPolicy />
-        },
-        {
-            path: "*",
-            element: <Navigate to="/" />
+            element: <RootLayout />,
+            children: [
+                {
+                    path: '/',
+                    element: <LandingPage />
+                },
+                {
+                    path: "/registro",
+                    element: <GuestRoute element={<SignIn />} />
+                },
+                {
+                    path: "/login",
+                    element: <GuestRoute element={<Login />} />
+                },
+                {
+                    path: "/reservar",
+                    element: <Reservas />
+                },
+                {
+                    path: "/perfil",
+                    element: <PrivateRoute element={<UserProfile />} />
+                },
+                {
+                    path: "/contacto",
+                    element: <ContactUs />
+                },
+                {
+                    path: "/terminos",
+                    element: <TermsandConds />
+                },
+                {
+                    path: "/politica-privacidad",
+                    element: <PrivacyPolicy />
+                },
+                {
+                    path: "*",
+                    element: <Navigate to="/" />
+                }
+            ]
         }
     ]
 );
